53d657efc6a47f88807e6cc58bf4f4f15214d1c4
Tj-wo <136597877+Tj-wo@users.noreply.github.com>
2024-08-30 11:54:47 +0300

    An update to the review page on frontend mentor

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
+
 const ratingStars = document.querySelectorAll('#rating .dot');
 const submitButton = document.getElementById('submit-btn');
-const messageElement = document.getElementById('message');
+const messageElement = document.getElementById('selection');
 let selectedRating = null;
 
 ratingStars.forEach(star => {
-    star.addEventListener('click', () => {
-        selectedRating = star.dataset.value;
-        ratingStars.forEach(star => star.classList.remove('selected'));
-        star.classList.add('selected');
-    });
+  star.addEventListener('click', () => {
+    selectedRating = star.dataset.value;
+    ratingStars.forEach(star => star.classList.remove('selected'));
+    star.classList.add('selected');
+  });
 });
 
 submitButton.addEventListener('click', () => {
-    if (selectedRating) {
-        // messageElement.textContent = `Thank you for your rating! You selected ${selectedRating} out of 5.`;
-        // messageElement.style.display = 'block';
-
-        
-
-
-
-    } else {
-        alert('Please select a rating.');
-    }
+  if (selectedRating) {
+    const message = `You selected ${selectedRating} out of 5.`;
+    window.location.href = `thankyou.html?message=${encodeURIComponent(message)}`;
+  } else {
+    alert('Please select a rating.');
+  }
 });
